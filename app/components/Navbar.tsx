89f68c51b7f5028aa6f40209a705fabc5c62e6b4
rudnam <francisalbarracin2@gmail.com>
2024-07-16 12:40:52 +0800

    Update navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,48 @@
 import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import upseal from "../images/UP-seal.png";
+import ctidlogo from "../images/CTID-logo.png";
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
-      <Link href="/" className="font-bold text-3xl text-primary">
-        CTID
-      </Link>
-      <Link href="/test" className="text-xl">
-        Test
-      </Link>
+    <nav className="w-full relative flex items-center justify-between max-w-4xl mx-auto px-4 py-5">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center">
+          <a href="/">
+            <Image src={upseal} alt="image" width={100} height={100} />
+          </a>
+          <a href="/">
+            <Image src={ctidlogo} alt="image" width={100} height={100} />
+          </a>
+        </div>
+        <div className="flex flex-col max-w-lg">
+          <Link href="/" className="font-bold text-2xl">
+            Department of Clothing, Textiles, and Interior Design
+          </Link>
+          <div>University of the Philippines Diliman</div>
+        </div>
+      </div>
+      <div className="flex items-center">🔍 Search f w</div>
+      {/* <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu> */}
     </nav>
   );
 }
