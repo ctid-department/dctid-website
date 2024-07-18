485444cd763298a20ad86dd397222b45393698a9
Aikiro42 <hiroku042@gmail.com>
2024-07-18 14:35:09 +0800

    fix: update hero component, add example in page.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,17 +1,30 @@
-import heroImage from "../images/Hero-image.jpg";
+import defaultHeroImage from "../images/sample1.png";
+import { cn } from "@/lib/utils";
 
-export default async function Hero() {
-  // TODO: should use background-image instead https://www.w3schools.com/howto/howto_css_parallax.asp
+export default async function Hero({...props}) {
   // TODO: try parallax effect? https://blog.logrocket.com/create-parallax-scrolling-css/
-  
-  console.log(heroImage.src)
+  // REFERENCE: https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+
+  console.log(defaultHeroImage)
+  console.log(props)
+
+  const heroCSS = cn(
+    "bg-fixed bg-center bg-contain bg-no-repeat w-full overflow-hidden flex flex-col justify-between",
+    props.className
+  )
+
   return (
     <div style={{
       backgroundColor: 'maroon',
-      backgroundImage: `url(${heroImage.src})`
+      backgroundImage: `url(${props.src ?? defaultHeroImage.src})`,
     }}
-    className="bg-fixed bg-center bg-no-repeat bg-cover w-full h-screen overflow-hidden flex flex-col justify-between">
+    className={heroCSS}>
       <div className="w-full h-0 shadow-[0_0_15px_15px_rgba(0,0,0,0.5)]"></div>
+      <div style={{
+        display: 'initial'
+      }}>
+        {props.children}
+      </div>
       <div className="w-full h-0 shadow-[0_0_15px_15px_rgba(0,0,0,0.5)]"></div>
     </div>
   );
