3856e1eb9ee0b7eeaf790e4aad03869bbdfcf1c5
Aikiro42 <hiroku042@gmail.com>
2024-07-18 09:50:15 +0800

    init: add simple hero image implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
+import HeroImage from ".//images/Hero-image.jpg";
 import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <main className="max-w-2xl mx-auto px-4">{children}</main>
+        <Image src={HeroImage} alt="UPD Oblation" width={0} height={0} sizes="100vw 100vh" className="w-screen h-[600px] fixed top-0 object-cover bg-rose-700 z-[-999]"/>
+        <div className="relative top-[50vh] w-screen bg-black p-[1%]" >
+          <main className="max-w-2xl mx-auto px-4">{children}</main>
+        </div>
       </body>
     </html>
   );
