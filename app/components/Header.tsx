a822f56f46cf49864bfa47c220964fd8a5a28f19
rudnam <francisalbarracin2@gmail.com>
2024-07-22 10:41:20 +0800

    Limit header width

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ import { MdSearch } from "react-icons/md";
 
 export default async function Header() {
   return (
-    <header className="bg-ctid-green">
+    <header className="bg-ctid-green text-white">
       <div className="max-w-6xl w-full flex flex-col items-center mx-auto px-4 py-4 md:flex-row justify-between ">
         <div className="flex flex-col md:flex-row items-center gap-4 mb-4 md:mb-0">
           <div className="flex items-center">
@@ -19,8 +19,8 @@ export default async function Header() {
               <Image src={ctidlogo} alt="CTID logo" width={100} height={100} />
             </Link>
           </div>
-          <div className="flex flex-col items-center md:items-start max-w-lg text-center md:text-left text-white">
-            <Link href="/" className="font-bold text-xl md:text-xl uppercase">
+          <div className="flex flex-col items-center md:items-start max-w-lg text-center md:text-left">
+            <Link href="/" className="font-bold text-xl uppercase">
               Department of Clothing, Textiles, and Interior Design
             </Link>
             <div className="text-sm uppercase">
@@ -29,12 +29,9 @@ export default async function Header() {
           </div>
         </div>
         <div className="flex items-center gap-5">
-          <MdSearch size={24} className="hover:text-gray-600 cursor-pointer" />
+          <MdSearch size={24} className="cursor-pointer" />
           <Link href="https://www.facebook.com/UPCTID/">
-            <FaFacebook
-              size={24}
-              className="hover:text-gray-600 cursor-pointer"
-            />
+            <FaFacebook size={24} className="cursor-pointer" />
           </Link>
         </div>
       </div>
