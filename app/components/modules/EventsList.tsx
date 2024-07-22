bce42a1e4882c4eb7eaf81cb09f05c06d51abea0
rudnam <francisalbarracin2@gmail.com>
2024-07-22 11:54:21 +0800

    Adjust events module margin

--- app/components/modules/EventsList.tsx
+++ app/components/modules/EventsList.tsx
@@ -40,7 +40,7 @@ const EventsList: React.FC<Props> = async ({ display }) => {
 
   return (
     <section className="my-8">
-      <div className="mx-auto max-w-3xl flex flex-col my-16">
+      <div className="mx-auto max-w-3xl flex flex-col my-8">
         {data.map((event: any, idx: number) => (
           <div key={idx} className="flex gap-4 h-16 items-stretch">
             <div className="flex items-center w-28 flex-shrink-0">
