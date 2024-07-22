f749ec8dfa5ab268430d018e4849198867c81961
rudnam <francisalbarracin2@gmail.com>
2024-07-22 10:28:27 +0800

    Limit header width

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,33 +8,35 @@ import { MdSearch } from "react-icons/md";
 
 export default async function Header() {
   return (
-    <header className="w-full flex flex-col items-center mx-auto px-4 py-4 pb-2 md:flex-row justify-between bg-ctid-green">
-      <div className="flex flex-col md:flex-row items-center gap-4 mb-4 md:mb-0">
-        <div className="flex items-center">
-          <Link href="/">
-            <Image src={upseal} alt="UP seal" width={80} height={80} />
-          </Link>
-          <Link href="/">
-            <Image src={ctidlogo} alt="CTID logo" width={100} height={100} />
-          </Link>
-        </div>
-        <div className="flex flex-col items-center md:items-start max-w-lg text-center md:text-left text-white">
-          <Link href="/" className="font-bold text-xl md:text-xl uppercase">
-            Department of Clothing, Textiles, and Interior Design
-          </Link>
-          <div className="text-sm uppercase">
-            University of the Philippines Diliman
+    <header className="bg-ctid-green">
+      <div className="max-w-6xl w-full flex flex-col items-center mx-auto px-4 py-4 md:flex-row justify-between ">
+        <div className="flex flex-col md:flex-row items-center gap-4 mb-4 md:mb-0">
+          <div className="flex items-center">
+            <Link href="/">
+              <Image src={upseal} alt="UP seal" width={80} height={80} />
+            </Link>
+            <Link href="/">
+              <Image src={ctidlogo} alt="CTID logo" width={100} height={100} />
+            </Link>
+          </div>
+          <div className="flex flex-col items-center md:items-start max-w-lg text-center md:text-left text-white">
+            <Link href="/" className="font-bold text-xl md:text-xl uppercase">
+              Department of Clothing, Textiles, and Interior Design
+            </Link>
+            <div className="text-sm uppercase">
+              University of the Philippines Diliman
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex items-center gap-5">
-        <MdSearch size={24} className="hover:text-gray-600 cursor-pointer" />
-        <Link href="https://www.facebook.com/UPCTID/">
-          <FaFacebook
-            size={24}
-            className="hover:text-gray-600 cursor-pointer"
-          />
-        </Link>
+        <div className="flex items-center gap-5">
+          <MdSearch size={24} className="hover:text-gray-600 cursor-pointer" />
+          <Link href="https://www.facebook.com/UPCTID/">
+            <FaFacebook
+              size={24}
+              className="hover:text-gray-600 cursor-pointer"
+            />
+          </Link>
+        </div>
       </div>
     </header>
   );
