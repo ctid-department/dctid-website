bcacd4025345c22aa40c788ad11608da46073c0d
Audrey Vianca Maegan Sy <112850909+audrey-vmsy@users.noreply.github.com>
2024-07-22 15:20:00 +0800

    fix: Edited Events timeline for mobile view (#15)
    Co-authored-by: audrey-vmsy <audreyvmsy@DESKTOP-5RMDU3H>
    Co-authored-by: Francis A. <francisalbarracin2@gmail.com>

--- app/components/modules/EventsList.tsx
+++ app/components/modules/EventsList.tsx
@@ -42,7 +42,7 @@ const EventsList: React.FC<Props> = async ({ display }) => {
     <section className="my-8">
       <div className="mx-auto max-w-3xl flex flex-col my-8">
         {data.map((event: any, idx: number) => (
-          <div key={idx} className="flex gap-4 h-16 items-stretch">
+          <div key={idx} className="flex gap-4 items-stretch">
             <div className="flex items-center w-28 flex-shrink-0">
               <div className="my-auto w-full text-center">
                 {formatDate(event.date)}
