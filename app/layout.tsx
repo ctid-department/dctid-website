47803f841474a31993565fc565113c7b50a89d72
Francis A. <francisalbarracin2@gmail.com>
2024-07-23 12:38:45 +0800

    Add Hero image (#19)

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-dvh`}>
+      <body
+        className={`${inter.className} flex flex-col min-h-dvh relative overflow-x-hidden`}
+      >
         <Header />
         <Navbar />
         <main className="max-w-4xl mx-auto px-4 flex-grow">{children}</main>
