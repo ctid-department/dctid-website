a7d39b886ea48a6f50af4bef57a63f7c4a4d6d16
Aikiro42 <hiroku042@gmail.com>
2024-07-25 16:10:06 +0800

    fix: improve mobile navigation dropdown

--- app/components/NavbarWrapper.tsx
+++ app/components/NavbarWrapper.tsx
@@ -11,21 +11,27 @@ export default function NavbarWrapper({...props}){
   function handleClick(){
     setNavbarHidden(!navbarHidden);
   }
-
+  
   const burgerCSS = cn(
-    "w-full bg-ctid-green block bg-center bg-contain md:hidden text-white",
-    "h-[10vh]"
+    "bg-ctid-green block bg-center bg-contain md:hidden text-white",
+    "w-[65px] h-[65px] rounded-full float-right relative top-[-65px] "
+  )
+
+  const childrenCSS = cn(
+    navbarHidden ? "hidden h-0 md:h-auto md:block" : "h-auto block",
+    "bg-ctid-green md:bg-transparent text-white md:text-black",
+    "transition-all"
+    // navbarHidden ? "hidden md:block bg-ctid-green md:bg-transparent text-white md:text-black" : "block bg-ctid-green text-white"
   )
 
   return (
     <div className="">
-      <div className={navbarHidden ? "hidden md:block bg-ctid-green md:bg-transparent text-white md:text-black" : "block bg-ctid-green text-white"}>
-        {props.children}
-      </div>
       <button className={burgerCSS} onClick={handleClick}>
-        {navbarHidden ? "Navigation" : ""}
         <ChevronDown className={`m-auto text-white ${navbarHidden ? "" : "rotate-180"}`}/>
       </button>
+      <div className={childrenCSS}>
+        {props.children}
+      </div>
     </div>
   )
 }
