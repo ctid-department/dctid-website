32c300ebc7b19c71f187b9362b31a7c81fe9b698
Aikiro42 <hiroku042@gmail.com>
2024-07-31 23:15:43 +0800

    style: improve FeatureList desktop CSS

--- app/components/FeatureList/FeatureItem.tsx
+++ app/components/FeatureList/FeatureItem.tsx
@@ -8,18 +8,28 @@ export default function FeatureItem({...props}){
   const CSS = {
     articleItem: cn(
       "flex flex-row items-center",
+      "my-1",
+      "md:flex-col md:justify-center",
+      "md:flex-grow",
+      "hover:bg-gray-200",
+      "transition-all"
     ),
     articleItemImage: cn(
-      "w-1/3 h-[10vh] my-2 mr-2",
-      "shadow-lg"
+      "w-1/2 h-[15vh]",
+      "md:w-[90%]",
+      "my-1 mr-2 md:mx-auto",
+      "shadow-md"
     ),
-    articleItemHeader: cn("font-bold")
+    articleItemHeader: cn(
+      "w-[90%]",
+      "font-semibold"
+    )
   }
 
   return(
   <Link className={CSS.articleItem} href={props.href ?? ""}>
     <Image className={CSS.articleItemImage} src="" width={0} height={0} alt=""/>
-    <h2 className={CSS.articleItemHeader}>{"Lorem Ipsum"}</h2>
+    <h2 className={CSS.articleItemHeader}>{"BSID Graduates Join the 1st Gabi ng Parangal Para sa mga Alumni 2024"}</h2>
   </Link>
 )
 }
--- app/components/FeatureList/index.tsx
+++ app/components/FeatureList/index.tsx
@@ -18,53 +18,75 @@ const FeatureList: React.FC<Props> = ({
   articles,
 }) => {
 
-  const sampleArticles = [0, 0, 0, 0, 0]
+  const sampleArticles = [0, 0, 0, 0]
 
   const CSS = {
     wrapper: cn(
       "w-full p-4 my-4",
     ),
-    featuredWrapper: cn(""),
+    featuredWrapper: cn(
+      "md:flex",
+      "md:pb-2"
+    ),
     featuredImage: cn(
       "w-full h-[25vh]",
+      "md:w-2/3 md:h-[50vh]",
+      "",
       "shadow-lg",
       "rounded-sm"
     ),
+    featuredInfo: cn(
+      "mb-4",
+      "md:w-1/3 md:pl-3"
+    ),
     featuredHeader: cn(
       "font-bold text-xl",
-      "mt-2"
+      "mt-2",
     ),
     featuredIntro: cn(
       "text-sm",
-      "mt-2 mb-4"
+      "mt-2",
+    ),
+    readMore: cn(
+      "text-sm text-ctid-taupe"
     ),
 
     articleList: cn(
       "pt-2",
-      "border-t border-ctid-taupe"
+      "border-t border-ctid-taupe",
+      "md:flex md:flex-row md:items-center"
     ),
 
     viewAllButton: cn(
-      "block w-full p-4 mt-2",
-      "shadow-lg border border-gray-200",
-      "text-center text-sm text-ctid-taupe font-semi"
+      "block p-4 mt-2 md:mt-0",
+      "md:h-full",
+      "border border-gray-200",
+      "md:border-0",
+      "text-center text-ctid-taupe font-semibold",
+      "align-middle",
+      "hover:underline hover:bg-gray-200",
+      "active:bg-ctid-taupe active:text-white",
+      "transition-all"
     ),
   }
 
   return (<div className={CSS.wrapper}>
-    <div className={CSS.featuredWrapper}>
+    <Link href="" className={CSS.featuredWrapper}>
       <Image className={CSS.featuredImage} src={""} width={0} height={0} alt=""/>
-      <h1 className={CSS.featuredHeader}>{"BSID Graduates Join the 1st Gabi ng Parangal Para sa mga Alumni 2024"}</h1>
-      <p className={CSS.featuredIntro}>{"Associate Professor and former Dean of the UP College of Home Economics (CHE), Dr. Adelaida Mayo, was honored with the first-ever Home Economics Lifetime Achievement Award by the Philippine Association for Technology in Home Economics of State Colleges..."} <span className={"text-ctid-taupe"} >Read more</span></p>
-    </div>
+      <div className={CSS.featuredInfo}>
+        <h1 className={CSS.featuredHeader}>{"BSID Graduates Join the 1st Gabi ng Parangal Para sa mga Alumni 2024"}</h1>
+        <p className={CSS.featuredIntro}>{"Associate Professor and former Dean of the UP College of Home Economics (CHE), Dr. Adelaida Mayo, was honored with the first-ever Home Economics Lifetime Achievement Award by the Philippine Association for Technology in Home Economics of State Colleges..."}</p>
+        <span className={CSS.readMore} >Read more</span>
+      </div>
+    </Link>
     <div className={CSS.articleList}>
       {
         sampleArticles.map((item, idx)=>(
           <FeatureItem articleItem={item} key={idx} />
         ))
       }
+      <Link className={CSS.viewAllButton} href="">View All ›</Link>
     </div>
-    <Link className={CSS.viewAllButton} href="">View All</Link>
   </div>)
 }
 
